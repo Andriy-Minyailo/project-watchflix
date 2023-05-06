2a84f40d3512b56ac548ba65179376b7bcd5c8c5
Ilya Kulish <ilyakulish06@outlook.com>
2023-05-07 03:44:18 +0400

    localstorage
    нужна работа над ним

---
+++ src/js/localStorage.js
@@ -0,0 +1,85 @@
+import { markup } from "./markup.js";
+import { RequestServer } from "./requestServer.js";
+import { requestGenre } from "./nameGenres.js";
+
+const requestServer = new RequestServer();
+
+async function loadMovies() {
+  await requestGenre();
+
+  const response = await requestServer.newFilms();
+  const { results } = response.data;
+
+  const movieList = results.map(({
+    poster_path,
+    original_title = "",
+    genre_ids,
+    overview = "",
+    release_date = "",
+    vote_average = "",
+    id,
+  }) => ({
+    poster_path,
+    original_title,
+    genre_ids,
+    overview,
+    release_date,
+    vote_average,
+    id,
+  }));
+
+  saveMovieList(movieList);
+
+  const markupCard = markup(movieList);
+  const movieContainer = document.querySelector(".movie__container");
+  if (movieContainer) {
+    movieContainer.innerHTML = markupCard;
+  }
+}
+
+function saveMovieList(movieList) {
+  localStorage.setItem("movieList", JSON.stringify(movieList));
+}
+
+function getMovieList() {
+  const movieList = localStorage.getItem("movieList");
+  if (movieList) {
+    return JSON.parse(movieList);
+  }
+  return [];
+}
+
+function addMovie() {
+  // Добавляем новый фильм в список фильмов
+  const newMovie = {
+    poster_path: "new-poster.jpg",
+    original_title: "New Movie",
+    genre_ids: [1, 2, 3],
+    overview: "Lorem ipsum dolor sit amet.",
+    release_date: "2023-05-07",
+    vote_average: 8.5,
+    id: 123456,
+  };
+
+  const movieList = getMovieList();
+  movieList.push(newMovie);
+
+  // Сохраняем обновленный список фильмов в localStorage
+  saveMovieList(movieList);
+
+  // Обновляем разметку страницы
+  const markupCard = markup(movieList);
+  const movieContainer = document.querySelector(".movie__container");
+  if (movieContainer) {
+    movieContainer.innerHTML = markupCard;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadMovies();
+
+  const addButton = document.querySelector(".add__button");
+  if (addButton) {
+    addButton.addEventListener("click", addMovie);
+  }
+});
