849cdbcc726b8e1e83cbe9db2f0977524f412ca6
OksanaKozak <oksanakozak2016@gmail.com>
2023-05-07 00:30:43 +0300

    add

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,28 +1,39 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+// (() => {
+//   const menu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+//   function toggleMenu() {
+//     menu.classList.toggle('visually-hidden');
+//   }
+  
+// })();
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+// ---------------------------------
+
+const refs = {
+  menuBtn: document.querySelector('[data-menu-open]'),
+  menuContainer: document.querySelector('[data-menu]'),
+  menuBackdrop: document.querySelector('[data-backdrop]'),
+  body: document.querySelector('body'),
+};
+
+refs.menuBtn.addEventListener('click', toggleModal);
+
+refs.menuBackdrop.addEventListener('click', closeOnBackdropClick);
+
+function closeOnBackdropClick(e) {
+  if (e.target === refs.menuBackdrop) {
+    toggleModal();
+  }
+}
+
+function toggleModal() {
+  refs.menuBackdrop.classList.toggle('visually-hidden');
+  refs.menuContainer.classList.toggle('is-open');
+  refs.body.classList.toggle('is-open');
+}
--- src/js/toggel.js
+++ src/js/toggel.js
@@ -1,19 +1,21 @@
 const toggleThemeBtn = document.getElementById('toggel-btn')
-const toggleThemeImage = document.getElementById('toggel-btn')
+const toggleThemeImage = document.getElementById('toggel-svg')
 
 toggleThemeBtn.addEventListener('click', () => {
    if (document.body.classList.contains('light')) {
    document.body.classList.remove('light')
-   toggleThemeImage.use = './svg/symbol-defs.svg#icon-sund'
+   toggleThemeImage.use ='./svg/symbol-defs.svg#icon-sun'
    localStorage.theme = 'dark'
    } else {
    document.body.classList.add('light')
-   toggleThemeImage.use = './svg/symbol-defs.svg#icon-moond'
+   toggleThemeImage.use ='./svg/symbol-defs.svg#icon-moon'
    localStorage.theme = 'light'
    }
 })
 
 if(localStorage.theme === 'light') {
    document.body.classList.add('light')
-   toggleThemeImage.use = './svg/symbol-defs.svg#icon-moond'
+   toggleThemeImage.use ='./svg/symbol-defs.svg#icon-moon'
 }
+
+
