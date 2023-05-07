1a93f6f6e75b06ab357cbe77a3853a37384c8b4f
Ilya Kulish <ilyakulish06@outlook.com>
2023-05-08 03:34:42 +0400

    button done but need work with light mode switcher

--- src/js/toggel.js
+++ src/js/toggel.js
@@ -1,21 +1,49 @@
-const toggleThemeBtn = document.getElementById('toggel-btn')
-const toggleThemeImage = document.getElementById('toggel-svg')
+// const toggleThemeBtn = document.getElementById('toggel-btn')
+// const toggleThemeImage = document.getElementById('toggel-svg')
+
+// toggleThemeBtn.addEventListener('click', () => {
+//    if (document.body.classList.contains('light')) {
+//    document.body.classList.remove('light')
+//    toggleThemeImage.use ='./svg/symbol-defs.svg#icon-sun'
+//    localStorage.theme = 'dark'
+//    } else {
+//    document.body.classList.add('light')
+//    toggleThemeImage.use ='./svg/symbol-defs.svg#icon-moon'
+//    localStorage.theme = 'light'
+//    }
+// })
+
+// if(localStorage.theme === 'light') {
+//    document.body.classList.add('light')
+//    toggleThemeImage.use ='./svg/symbol-defs.svg#icon-moon'
+// }
+// старый код выше
+
+// пытался поставить новый код для лайт темы можешь попробовать он не переключает с темной темы на светлую 
+const toggleThemeBtn = document.getElementById('toggle-btn');
+const toggleThemeImage = document.querySelectorAll('.icon-sun_button use, .icon-sun_moon use');
 
 toggleThemeBtn.addEventListener('click', () => {
-   if (document.body.classList.contains('light')) {
-   document.body.classList.remove('light')
-   toggleThemeImage.use ='./svg/symbol-defs.svg#icon-sun'
-   localStorage.theme = 'dark'
-   } else {
-   document.body.classList.add('light')
-   toggleThemeImage.use ='./svg/symbol-defs.svg#icon-moon'
-   localStorage.theme = 'light'
-   }
-})
+  if (toggleThemeBtn.classList.contains('dark')) {
+    toggleThemeBtn.classList.remove('dark');
+    toggleThemeImage.forEach((icon) => {
+      icon.setAttribute('href', './svg/symbol-defs.svg#icon-sun_moon');
+    });
+    localStorage.theme = 'light';
+  } else {
+    toggleThemeBtn.classList.add('dark');
+    toggleThemeImage.forEach((icon) => {
+      icon.setAttribute('href', './svg/symbol-defs.svg#icon-sun_button');
+    });
+    localStorage.theme = 'dark';
+  }
+});
 
-if(localStorage.theme === 'light') {
-   document.body.classList.add('light')
-   toggleThemeImage.use ='./svg/symbol-defs.svg#icon-moon'
+if (localStorage.theme === 'dark') {
+  toggleThemeBtn.classList.add('dark');
+  toggleThemeImage.forEach((icon) => {
+    icon.setAttribute('href', './svg/symbol-defs.svg#icon-sun_button');
+  });
 }
 
 
