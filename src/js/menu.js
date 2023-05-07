fc3a739b9cc710a87e07884779688e46cd666ffb
OksanaKozak <oksanakozak2016@gmail.com>
2023-05-07 10:53:40 +0300

    add

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,39 +1,23 @@
-// (() => {
-//   const menu = document.querySelector('.js-menu-container');
-//   const openMenuBtn = document.querySelector('.js-open-menu');
-//   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-//   openMenuBtn.addEventListener('click', toggleMenu);
-//   closeMenuBtn.addEventListener('click', toggleMenu);
+(() => {
+  const menu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
 
-//   function toggleMenu() {
-//     menu.classList.toggle('visually-hidden');
-//   }
+  function toggleMenu() {
+    menu.classList.toggle('visually-hidden');
+  }
   
-// })();
+})();
 
 // ---------------------------------
 
-const refs = {
-  menuBtn: document.querySelector('[data-menu-open]'),
-  menuContainer: document.querySelector('[data-menu]'),
-  menuBackdrop: document.querySelector('[data-backdrop]'),
-  body: document.querySelector('body'),
-};
-
-refs.menuBtn.addEventListener('click', toggleModal);
-
-refs.menuBackdrop.addEventListener('click', closeOnBackdropClick);
-
-function closeOnBackdropClick(e) {
-  if (e.target === refs.menuBackdrop) {
-    toggleModal();
-  }
-}
-
-function toggleModal() {
-  refs.menuBackdrop.classList.toggle('visually-hidden');
-  refs.menuContainer.classList.toggle('is-open');
-  refs.body.classList.toggle('is-open');
-}
+// document.addEventListener("DOMContentLoaded", function() {
+//   document.getElementById("header-menu").addEventListener("click", function() {
+//     document.querySelector("header-menu").classList.toggle("open")
+//   })
+// })
