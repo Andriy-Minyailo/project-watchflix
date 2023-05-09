a2953d43edfc7d7794887d4e67d777673a46127f
Gorodeckii912 <d.gavrilenko912@gmail.com>
2023-05-09 17:52:12 +0100

    33333

--- src/index.js
+++ src/index.js
@@ -4,33 +4,14 @@ import './js/modal';
 import './js/toggel';
 import './js/menu';
 
-import './js/modals-open-close';
-import './js/modalTrailer';
 import './js/hero';
 
 import './js/weekly-trends';
 
 import './js/upcoming';
 
-import './js/modals-open-close';
-
 
 
 import './js/localModalJs';
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import './js/footerModal';
