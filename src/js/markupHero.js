030fd7d12dc9265db1167103cdfe92a571033778
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-05-09 13:24:34 +0300

    Update markupHero.js

--- src/js/markupHero.js
+++ src/js/markupHero.js
@@ -1,24 +1,26 @@
 export function markupHero(results) {
- console.log(results[2])
-  const { backdrop_path,
+  console.log(results[2]);
+  const {
+    backdrop_path,
     poster_path,
     original_title = '',
     overview = '',
     release_date = '',
     vote_average = '',
-    id } = results[1];
-        let releaseDate;
-        if (!release_date) {
-          releaseDate = 'no date';
-        } else {
-          releaseDate = release_date.substring(0, 4);
-        }
-        let path = `https://image.tmdb.org/t/p/w500${poster_path}`;
-        if (!poster_path) {
-          path = 'https://picsum.photos/500/750';
-        }
-      let rating = vote_average.toFixed(1);
-        return `<div class="hero__discription">
+    id,
+  } = results[1];
+  let releaseDate;
+  if (!release_date) {
+    releaseDate = 'no date';
+  } else {
+    releaseDate = release_date.substring(0, 4);
+  }
+  let path = `https://image.tmdb.org/t/p/w500${poster_path}`;
+  if (!poster_path) {
+    path = 'https://picsum.photos/500/750';
+  }
+  let rating = vote_average.toFixed(1);
+  return `<div class="hero__discription">
             <h2 class="hero__title">${original_title}</h2>
             <p class="hero__rating">${rating}</p>
             <p class="hero__text">${overview}</p>
@@ -34,5 +36,4 @@ export function markupHero(results) {
             <div class="hero__backgr" id="${id}">
             <img src='${path}' loading="lazy" alt="${overview}" class="hero__img" />
             </div>`;
-
 }
