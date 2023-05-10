213a2c596175366b7b1c4a1e8cfa6f46fb1e720f
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-05-10 14:29:04 +0300

    fix-catalog

--- src/catalog.js
+++ src/catalog.js
@@ -2,39 +2,8 @@ import './js/modal';
 import './js/toggel';
 import './js/elementCard';
 import './js/hero';
-
-
-
 import './js/localModalJs';
-
 import './js/catalogFilter';
-
-
-import { startRenderModalInfo } from './js/createModalInfo/createModalInfo';
-
-
-
-startRenderModalInfo(603692);
-
-
 import './js/modals-open-close';
 import './js/modalTrailer';
-import './js/hero';
-
-import './js/weekly-trends';
-
-// import './js/upcoming';
-
-import './js/modals-open-close';
-
-
-
-import './js/localModalJs';
-
-
-
-
-
-
-
 import './js/footerModal';
--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,17 +1,6 @@
 import './js/modal';
 import './js/toggel';
 import './js/localStorage'
-
 import './js/checkLocalStorage'
-// при клике на кнопку переход на каталог 
-// const searchMovieBtn = document.getElementById("search-movie-btn");
-
-// searchMovieBtn.addEventListener("click", function() {
-//   window.location.replace("catalog.html");
-// });
-
-
-
 import './js/localModalJs';
-
 import './js/modals-open-close';
