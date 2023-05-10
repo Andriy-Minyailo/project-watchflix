23d8aa7b4724d271668f88fdb40935c7ee872664
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-05-10 23:17:48 +0300

    fix lightTema

--- src/js/weekly-trends.js
+++ src/js/weekly-trends.js
@@ -2,7 +2,6 @@ import { RequestServer } from './requestServer';
 import { markup } from './markup';
 import { requestGenre } from './nameGenres';
 
-
 const refs = {
   weeklyCard: document.querySelector('.card'),
 };
@@ -16,7 +15,16 @@ async function weeklyTrend() {
     const markupCard = markup(results.slice(0, 3));
 
     refs.weeklyCard.innerHTML = markupCard;
-    
+
+    const weeklyTitle = document.querySelector('.weekly-tr__title');
+    let LightSwitcher = document.querySelector('.switcher');
+    let isLight = localStorage.getItem('isLight');
+    if (isLight === 'true') {
+      weeklyTitle.classList.add('weekly-tr__title-light');
+    }
+    LightSwitcher.addEventListener('click', () => {
+      weeklyTitle.classList.toggle('weekly-tr__title-light');
+    });
   } catch (error) {
     console.log(error);
   }
