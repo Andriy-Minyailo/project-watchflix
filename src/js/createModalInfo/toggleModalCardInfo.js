dc13b5f4b2367f4a702ca75a9a7fedd3a84b222f
Андрій <109473689+Andriy-Minyailo@users.noreply.github.com>
2023-05-11 22:15:57 +0300

    Revert "Modalfour"

--- src/js/createModalInfo/toggleModalCardInfo.js
+++ src/js/createModalInfo/toggleModalCardInfo.js
@@ -1,80 +1,73 @@
-import lightBtnClose from '../../svg/close.svg'
-import darkBtnClose from '../../svg/Vector.svg'
+// import lightBtnClose from '../../svg/close.svg'
+// import darkBtnClose from '../../svg/Vector.svg'
 
-let isLight = getLocalStorage('isLight') || 'false';
-const colorClass = "light"
-
-
-const utils = {
-    btnCls: findOneElement(".img-cls"),
-    popupContent: findOneElement(".popup__content"),
-    modalInfo: findOneElement(".modal__info"),
-    modalAbout: findOneElement(".modal-info__about"),
-    vote: findOneElement("._vote"),
-    votes: findOneElement("._votes"),
-    textLeft: findElements(".text-left"),
-    addToLocalStoradge: findOneElement(".add-to-my-library-black-btn-dark-theme"),
-}
+// const LightSwitcher = document.querySelector(".switcher");
+// let isLight = localStorage.getItem('isLight') || false;
+// const colorClass = "light"
 
 
 
-if (isLight === 'true') {
-    document.body.classList.add('light');
+// const utils = {
+//     btnCls: findOneElement(".img-cls"),
+//     popupContent: findOneElement(".popup__content"),
+//     modalInfo: findOneElement(".modal__info"),
+//     modalAbout: findOneElement(".modal-info__about"),
+//     vote: findOneElement("._vote"),
+//     votes: findOneElement("._votes"),
+//     textLeft: findElements(".text-left"),
+//     addToLocalStoradge: findOneElement(".add-to-my-library-black-btn-dark-theme"),
+// }
+
+
+// if (isLight === 'true') {
+    // document.body.classList.add('light');
     changeColorElementToLight()
-} else {
-    changeColorElementToDark()
-
-}
-
+// } else {
+//     deleteClassList(utils.popupContent, colorClass)
+// }
 
 
-export function toggleModalCardInfo() {
-    getLocalStorage('isLight') == "true" ? changeColorElementToLight() : changeColorElementToDark()
+// LightSwitcher.addEventListener("click", function() {
+//     isLight ? changeColorElementToLight() : changeColorElementToDark()
+// })
+
+
+// function changeColorElementToLight() {
+//     utils.btnCls.src = darkBtnClose;
     
-}
+//     addClassList(utils.popupContent, colorClass);
+//     addClassList(utils.modalInfo, colorClass);
+//     addClassList(utils.modalAbout, colorClass);
+//     addClassList(utils.vote, colorClass);
+//     addClassList(utils.votes, colorClass);
+//     addClassList(utils.addToLocalStoradge, colorClass);
+
+//     [...utils.textLeft].map(item => addClassList(item, colorClass))
+// }
+
+// function changeColorElementToDark() {
+//     utils.btnCls.src = lightBtnClose;
+//     deleteClassList(utils.popupContent, colorClass);
+//     deleteClassList(utils.modalInfo, colorClass);
+//     deleteClassList(utils.modalAbout, colorClass);
+//     deleteClassList(utils.vote, colorClass);
+//     deleteClassList(utils.votes, colorClass);
+//     deleteClassList(utils.addToLocalStoradge, colorClass);
+//     [...utils.textLeft].map(item => deleteClassListdeleteClassList(item, colorClass))
+// }
 
 
-function changeColorElementToLight() {
-    utils.btnCls.src = darkBtnClose;
-    
-    addClassList(utils.popupContent, colorClass);
-    addClassList(utils.modalInfo, colorClass);
-    addClassList(utils.modalAbout, colorClass);
-    addClassList(utils.vote, colorClass);
-    addClassList(utils.votes, colorClass);
-    addClassList(utils.addToLocalStoradge, colorClass);
-
-    [...utils.textLeft].map(item => addClassList(item, colorClass))
-}
-
-function changeColorElementToDark() {
-    utils.btnCls.src = lightBtnClose;
-    deleteClassList(utils.popupContent, colorClass);
-    deleteClassList(utils.modalInfo, colorClass);
-    deleteClassList(utils.modalAbout, colorClass);
-    deleteClassList(utils.vote, colorClass);
-    deleteClassList(utils.votes, colorClass);
-    deleteClassList(utils.addToLocalStoradge, colorClass);
-    [...utils.textLeft].map(item => deleteClassList(item, colorClass))
-}
+// function addClassList(element, className) {
+//     element.classList.add(className)
+// }
+// function deleteClassList(element, className) {
+//     element.classList.remove(className)
+// }
 
 
-function addClassList(element, className) {
-    element.classList.add(className)
-}
-function deleteClassList(element, className) {
-    element.classList.remove(className)
-}
-
-
-function findOneElement(selector) {
-    return document.querySelector(selector)
-}
-function findElements(selector) {
-    return document.querySelectorAll(selector)
-}
-
-
-function getLocalStorage(key) {
-    return localStorage.getItem(key)
-}
+// function findOneElement(selector) {
+//     return document.querySelector(selector)
+// }
+// function findElements(selector) {
+//     return document.querySelectorAll(selector)
+// }
