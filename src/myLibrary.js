4ec342c6b1d1cf68ed5a83474d34a15f49ccd989
Gorodeckii912 <d.gavrilenko912@gmail.com>
2023-05-11 19:36:59 +0100

    01

--- src/myLibrary.js
+++
@@ -1,7 +0,0 @@
-import './js/modal';
-import './js/toggel';
-import './js/localStorage'
-import './js/checkLocalStorage'
-import './js/localModalJs';
-import './js/modals-open-close';
-import './js/scrollUp';
